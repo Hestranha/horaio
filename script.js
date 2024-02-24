ab7340fa8590ad6330e078ee3f2856a1f2de3607
Hestranha <94768439+Hestranha@users.noreply.github.com>
2024-02-24 10:59:23 -0500

    ajustes especificos

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const colorInput = document.getElementById('color');
-colorInput.value = '#ffffff';
+generarColorPastel();
+function generarColorPastel() {
+    // Generar componentes de color RGB para un color pastel
+    var r = Math.floor(Math.random() * 100) + 155; // Rojo en el rango 155-255
+    var g = Math.floor(Math.random() * 100) + 155; // Verde en el rango 155-255
+    var b = Math.floor(Math.random() * 100) + 155; // Azul en el rango 155-255
 
+    // Convertir componentes RGB a formato hexadecimal
+    var colorHex = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+
+    // Establecer el valor en el elemento de entrada de color
+    var colorInput = document.getElementById('color');
+    colorInput.value = colorHex;
+
+}
 var ultimoValor;
 var contieneSeisO7;
-
+var idsCumplidos = [];
+var cursos = [];
+let indice = 1;
 function agregarCurso() {
     // Obtener los valores del formulario
     const nombreCurso = document.getElementById('nombre-curso').value;
@@ -15,27 +29,32 @@ function agregarCurso() {
     if (validacion() == 0) {
         return;
     }
-
+    generarColorPastel();
 
     var filas = document.getElementById('horario-mostrado').getElementsByTagName('tr');
-    var idsCumplidos = [];
+
+    //var ultimaHoraMenorInicio = '';
+    //var ultimaHoraMenorFin = '';
+
     for (var i = 1; i < filas.length; i++) {
         // Obtener la hora de inicio de la fila actual (asumiendo que está en el formato hh:mm)
         var horaFilaInicio = filas[i].querySelector('th').innerText.split(' - ')[0];
-
         // Obtener la hora de fin de la fila actual (asumiendo que está en el formato hh:mm)
         var horaFilaFin = filas[i].querySelector('th').innerText.split(' - ')[1];
-
-        // Verificar si la hora de inicio está dentro del rango
-        if (horaFilaInicio >= horaInicio && horaFilaInicio <= horaFin) {
-            console.log(filas[i].querySelector('th').id);
-            idsCumplidos.push(filas[i].querySelector('th').id);
-        } else {
-            // Verificar si la hora de fin está dentro del rango
-            if (horaFilaFin >= horaInicio && horaFilaFin <= horaFin) {
-                console.log(filas[i].querySelector('th').id);
-                idsCumplidos.push(filas[i].querySelector('th').id);
+        // Verificar si la hora de inicio o la hora de fin está dentro del rango
+        if (horaFilaFin >= horaInicio && horaFilaInicio <= horaFin) {
+            // Asignar la última hora menor que la hora de inicio y fin proporcionadas
+            /*
+            if (horaFilaInicio < horaInicio) {
+                ultimaHoraMenorInicio = horaFilaInicio;
             }
+            if (horaFilaFin < horaFin) {
+                ultimaHoraMenorFin = horaFilaFin;
+            }
+question    */
+            // Agregar la fila actual al array idsCumplidos
+            //console.log(filas[i].querySelector('th').id);
+            idsCumplidos.push(filas[i].querySelector('th').id);
         }
     }
     console.log('id maximo: ', idsCumplidos);
@@ -58,13 +77,23 @@ function agregarCurso() {
 
     // Imprimir los resultados en la consola
     console.log("Resultados:", resultados);
+    var nuevoCurso = {
+        "id": indice,
+        "nombre": nombreCurso,
+        "celdas": resultados,
+        "dias": diasSeleccionados,
+        "hora": [horaInicio, horaFin],
+    };
+    cursos.push(nuevoCurso);
+    indice += 1;
+    //console.log(nuevoCurso);
     // Bucle para iterar sobre los resultados
     for (var i = 0; i < resultados.length; i++) {
         var idResultado = resultados[i];
 
         // Obtener la celda correspondiente a la combinación de fila y día
         var celda = document.getElementById(idResultado);
-        console.log(colorSeleccionado);
+        //console.log(colorSeleccionado);
         // Verificar si la celda existe antes de actualizar su contenido
         if (celda) {
             // Actualizar el contenido de la celda con el nombre del curso
@@ -74,16 +103,113 @@ function agregarCurso() {
     }
 
     var longitudArray = idsCumplidos.length;
+    //console.log(longitudArray);
+    if (ultimoValor == null) {
+        ultimoValor = idsCumplidos[longitudArray - 1].replace('f', '');
+    } else {
+        aaa = idsCumplidos[longitudArray - 1].replace('f', '');
+        var numeroUltimoValor = parseFloat(ultimoValor);
+        var numeroAaa = parseFloat(aaa);
+        //console.log(aaa);
+        //console.log(ultimoValor);
+        if (numeroAaa > numeroUltimoValor) {
+            ultimoValor = aaa;
+        }
+    }
+    //ultimoValor = idsCumplidos[longitudArray - 1].replace('f', '');
+    //console.log(ultimoValor);
+    //console.log('valor final: ', ultimoValor);
 
-    ultimoValor = idsCumplidos[longitudArray - 1].replace('f', ''); // ultima Fila
-    console.log(ultimoValor);
     contieneSeisO7 = diasSeleccionados.includes(6) || diasSeleccionados.includes(7); // true o false
-    console.log(contieneSeisO7);
-    
+    //console.log(contieneSeisO7);
+
+    idsCumplidos.splice(0, idsCumplidos.length);
+    resultados.splice(0, resultados.length);
     //limpiar
-    document.getElementById('nombre-curso').value = "";
+    //document.getElementById('nombre-curso').value = "";
 
 }
+/*
+var cursos = {
+    curso1: {
+        id: 1,
+        nombre: "Curso 1",
+        celdas: 20,
+        dias: [1, 2],
+        hora: ["10:00 AM", "12:00 PM"]
+    },
+    curso2: {
+        id: 2,
+        nombre: "Curso 2",
+        celdas: 15,
+        dias: [3, 5],
+        hora: ["2:00 PM", "4:00 PM"]
+    }
+};
+*/
+//borrarCurso();
+function borrarCurso() {
+    let ventanaEmergente = document.getElementById('ventanaEmergente');
+    ventanaEmergente.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+    let infoCursosContainer = document.getElementById("infoCursos");
+    infoCursosContainer.innerHTML = "";
+    const nombresDias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+    for (let key in cursos) {
+        let curso = cursos[key];
+        if (cursos.hasOwnProperty(key)) {
+            // Obtener nombres de días en lugar de números
+            let nombresDiasSeleccionados = curso.dias.map(dia => nombresDias[dia - 1]);
+
+            infoCursosContainer.innerHTML += `
+            <div class="container-curso">
+                <h2 class="m-nombre-curso">- ${curso.nombre}</h2>
+                <p>Duración: ${curso.hora[0]} - ${curso.hora[1]}</p>
+                <p>Días: ${nombresDiasSeleccionados.join(', ')}</p>
+                <div class="xd">
+                    <input class="borrar-checkbox" type="checkbox" value="${curso.id}"/>
+                </div>
+            </div><hr>
+            `;
+        }
+    }
+    //infoCursosContainer.innerHTML += `<p>Celdas: ${curso.celdas}</p>`;
+
+    ventanaEmergente.addEventListener('click', function (event) {
+        if (event.target === ventanaEmergente) {
+            cerrarVentanaEmergente();
+        }
+    });
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            cerrarVentanaEmergente();
+        }
+    });
+}
+
+function cerrarVentanaEmergente() {
+    var ventanaEmergente = document.getElementById('ventanaEmergente');
+    ventanaEmergente.style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+
+function confirmarBorrarCurso() {
+    let checkboxes = document.querySelectorAll('.borrar-checkbox');
+    let cursosSeleccionados = [];
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener('change', function () {
+            if (this.checked) {
+                cursosSeleccionados.push(this.value);
+            } else {
+                cursosSeleccionados = cursosSeleccionados.filter(id => id !== this.value);
+            }
+        });
+    });
+    console.log(cursosSeleccionados);
+}
+
 function validacion() {
     const nombreCurso = document.getElementById('nombre-curso').value;
     const diasSeleccionados = obtenerDiasSeleccionados();
@@ -155,77 +281,97 @@ function obtenerHora(hora) {
 }
 
 function descargarHorario() {
-    if (validacion() == 0) {
+    if (cursos.length === 0) {
+        Swal.fire({
+            title: 'Error',
+            text: 'Ingrese al menos un curso',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        });
         return;
     }
-    var numeroUltimoValor = parseInt(ultimoValor, 10);  // 10 es la base numérica
-    numeroUltimoValor += 1;
-    console.log(typeof numeroUltimoValor);
-    for (var i = numeroUltimoValor; i <= 15; i++) {
-        var elemento = document.getElementById(i.toString());
-        console.log('aaaaaaaaaaaa');
-        if (elemento) {
-            elemento.style.display = 'none';
+    Swal.fire({
+        title: 'Advertencia',
+        text: 'El contenido se reiniciará',
+        icon: 'warning',
+        showCancelButton: true,  // Habilitar botón de cancelar
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar'  // Texto para el botón de cancelar
+    }).then((result) => {
+        if (!result.isConfirmed) {
+            console.log('Aceptar clicado');
+            return;
         }
-    }
-    if (!contieneSeisO7) {
-        // Ocultar elementos con IDs que contienen 'f6' o 'f7'
-        for (var i = 6; i <= 7; i++) {
-            for (var j = 1; j <= 15; j++) {
-                var elemento = document.getElementById('f' + j + '-' + i);
-                if (elemento) {
-                    elemento.style.display = 'none';
-                }
+        var numeroUltimoValor = parseInt(ultimoValor, 10);  // 10 es la base numérica
+        numeroUltimoValor += 1;
+        console.log(typeof numeroUltimoValor);
+        for (var i = numeroUltimoValor; i <= 15; i++) {
+            var elemento = document.getElementById(i.toString());
+            console.log('aaaaaaaaaaaa');
+            if (elemento) {
+                elemento.style.display = 'none';
             }
         }
-        var elemento = document.getElementById('s6');
-        if (elemento) {
-            elemento.style.display = 'none';
+        if (!contieneSeisO7) {
+            // Ocultar elementos con IDs que contienen 'f6' o 'f7'
+            for (var i = 6; i <= 7; i++) {
+                for (var j = 1; j <= 15; j++) {
+                    var elemento = document.getElementById('f' + j + '-' + i);
+                    if (elemento) {
+                        elemento.style.display = 'none';
+                    }
+                }
+            }
+            var elemento = document.getElementById('s6');
+            if (elemento) {
+                elemento.style.display = 'none';
+            }
+            var elemento = document.getElementById('d7');
+            if (elemento) {
+                elemento.style.display = 'none';
+            }
         }
-        var elemento = document.getElementById('d7');
-        if (elemento) {
-            elemento.style.display = 'none';
-        }
-    }
-    // Obtener el ancho y alto actual de la tabla
-    var anchoTabla = document.getElementById('miHorario').scrollWidth;
-    var altoTabla = document.getElementById('miHorario').scrollHeight;
+        // Obtener el ancho y alto actual de la tabla
+        var anchoTabla = document.getElementById('miHorario').scrollWidth;
+        var altoTabla = document.getElementById('miHorario').scrollHeight;
 
-    // Verificar si es un dispositivo móvil (ancho de la ventana menor que 768 píxeles)
-    var esDispositivoMovil = window.innerWidth < 768;
+        // Verificar si es un dispositivo móvil (ancho de la ventana menor que 768 píxeles)
+        var esDispositivoMovil = window.innerWidth < 768;
 
-    // Configurar las opciones para html2canvas según el dispositivo
-    var opcionesHtml2Canvas = {
-        width: esDispositivoMovil ? anchoTabla : null, // Si es dispositivo móvil, ajustar el ancho al ancho de la tabla
-        height: esDispositivoMovil ? altoTabla : null, // Si es dispositivo móvil, ajustar la altura al alto de la tabla
-        scale: 2, // Ajustar la resolución (puedes aumentar o disminuir según sea necesario)
-        logging: true, // Habilitar el registro para ver si hay mensajes de error
-        allowTaint: true,
-        useCORS: true,
-        backgroundColor: null, // Puedes establecer un color de fondo si es necesario
-        imageTimeout: 0, // Deshabilitar el tiempo de espera para las imágenes
-        quality: 2, // Ajustar la calidad de la imagen (puedes ajustar según sea necesario)
-    };
+        // Configurar las opciones para html2canvas según el dispositivo
+        var opcionesHtml2Canvas = {
+            width: esDispositivoMovil ? anchoTabla : null, // Si es dispositivo móvil, ajustar el ancho al ancho de la tabla
+            height: esDispositivoMovil ? altoTabla : null, // Si es dispositivo móvil, ajustar la altura al alto de la tabla
+            scale: 2, // Ajustar la resolución (puedes aumentar o disminuir según sea necesario)
+            logging: true, // Habilitar el registro para ver si hay mensajes de error
+            allowTaint: true,
+            useCORS: true,
+            backgroundColor: null, // Puedes establecer un color de fondo si es necesario
+            imageTimeout: 0, // Deshabilitar el tiempo de espera para las imágenes
+            quality: 2, // Ajustar la calidad de la imagen (puedes ajustar según sea necesario)
+        };
 
-    // Utilizar html2canvas con las opciones configuradas
-    html2canvas(document.getElementById('miHorario'), opcionesHtml2Canvas).then(function (canvas) {
-        // Crear un enlace para la descarga
-        var enlaceDescarga = document.createElement('a');
+        // Utilizar html2canvas con las opciones configuradas
+        html2canvas(document.getElementById('miHorario'), opcionesHtml2Canvas).then(function (canvas) {
+            // Crear un enlace para la descarga
+            var enlaceDescarga = document.createElement('a');
 
-        // Convertir el canvas a una URL de datos (data URL)
-        var imagenURL = canvas.toDataURL('image/png');
+            // Convertir el canvas a una URL de datos (data URL)
+            var imagenURL = canvas.toDataURL('image/png');
 
-        // Asignar la URL de la imagen al enlace de descarga
-        enlaceDescarga.href = imagenURL;
+            // Asignar la URL de la imagen al enlace de descarga
+            enlaceDescarga.href = imagenURL;
 
-        // Asignar el nombre del archivo
-        enlaceDescarga.download = 'horario_con_estilo.png';
+            // Asignar el nombre del archivo
+            enlaceDescarga.download = 'horario_con_estilo.png';
 
-        // Simular un clic en el enlace para iniciar la descarga
-        enlaceDescarga.click();
-        location.reload();
+            // Simular un clic en el enlace para iniciar la descarga
+            enlaceDescarga.click();
+            location.reload();
+        });
     });
-    
+
+
 }
 
 
